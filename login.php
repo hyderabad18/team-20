e300d6b5df8135452c8b763f8d244351ed4f93a2
kolliparakavyasree <kolliparakavyasree@gmail.com>
2018-07-14 20:20:19 +0530

    login php

--- login.php
+++ login.php
@@ -1,30 +1,34 @@
 <?php
+
+@include('login.html');
+
+
 $con = mysqli_connect("localhost","root","","youth4jobs");
 if($con==false)
 {
 	die("Error" . mysqli_connect_error());
 }
-
-@include('index.html');
-
+echo "db connected";
 if(isset($_POST['submit']))
 {
-	
+	echo 'posted';
 	$mail=$_POST['email'];
 	$pass=$_POST['password'];
 	$flag=0;
-	
+	echo $mail;
+	echo 'took values';
 	$squery="SELECT * from student where smail='$mail' and spass='$pass'";
 	$sresult=mysqli_query($con,$squery);
 	$sres=mysqli_num_rows($sresult);
 	if($sres==TRUE)
 	{
+		echo "true";
 		$flag=1;
 		header('location:StudentPage.html');
 	}
-	
+	/*
 	$uquery="SELECT * from university where unmail='$mail' and unpass='$pass'";
-	$uresult=mysqli_query($conn,$uquery);
+	$uresult=mysqli_query($con,$uquery);
 	$ures=mysqli_num_rows($uresult);
 	if($ures==TRUE){
 			$flag=1;
@@ -32,7 +36,7 @@ if(isset($_POST['submit']))
 	}
 	
 	$aquery="SELECT * from admin where aduname='$mail' and adpass='$pass'";
-	$aresult=mysqli_query($conn,$aquery);
+	$aresult=mysqli_query($con,$aquery);
 	$ares=mysqli_num_rows($aresult);
 	if($ares==TRUE){
 		$flag=1;
@@ -40,7 +44,7 @@ if(isset($_POST['submit']))
 	}
 	
 	$cquery="SELECT * from corporate where corpmail='$mail' and corppass='$pass'";
-	$cresult=mysqli_query($conn,$cquery);
+	$cresult=mysqli_query($con,$cquery);
 	$cres=mysqli_num_rows($cresult);
 	if($cres==TRUE){
 		$flag=1;
@@ -51,7 +55,7 @@ if(isset($_POST['submit']))
 	  echo "<b>user is not valid.</b>";
 	  header('location:');
 	}
-	
+	*/
 }
 
 
